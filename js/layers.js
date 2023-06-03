3edbaf764c6bcfdd179553d5d4da1f0ca71d5bc9
HothUH33 <134778302+HothUH33@users.noreply.github.com>
2023-06-03 07:56:19 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1562,7 +1562,7 @@ addLayer("eter", {
 
     name: "Lore", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "L",
-    color() {return GlowingColor("#ff0000",10,"#00ff00",10,"#0000ff")},       // The color for this layer, which affects many elements.
+    color() {return GlowingColor("#aa0000",10,"#00aa00",10,"#0000aa")},       // The color for this layer, which affects many elements.
     type: "none",//不被重置
     row: "side",
     position:4,
