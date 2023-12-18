f9042e40e80f468216fbafc7ad60961eea96afe6
HothUH33 <134778302+HothUH33@users.noreply.github.com>
2023-12-18 19:54:32 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -3,5 +3,23 @@ addLayer("m", {
     symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     row: 10,
-    color: "#FFFFFF",
+    color: "rgba(255,255,255,0.1"),
+    tooltip: "Games",
+    clickables:{  
+        11: {
+            display() {
+                return a+cost
+            },
+            canClick() {return true},
+            unlocked() {return true}, 
+            onClick() {window.location.assign("https://hothuh33.github.io/The-ExisReality-Tree")},
+            style: {"min-height"(){return "150px"}, 
+                    "width"(){return "200px"},
+	                  "border": "4px solid",
+	                  "border-color"(){return "rgba(0, 0, 0, 0.5)"}, 
+                    "color"(){return "#aaaaaa"},
+                    "background-color"(){return "#442244"},
+           },
+        },
+    },
 })
