89ee19f01f9bb107bb763590c8d59a0677685333
HothUH33 <134778302+HothUH33@users.noreply.github.com>
2023-12-18 20:01:23 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -3,7 +3,7 @@ addLayer("m", {
     symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     row: 10,
-    color: "rgba(255,255,255,0.1"),
+    color: "rgba(255,255,255,0.1)",
     tooltip: "Games",
     clickables:{  
         11: {
